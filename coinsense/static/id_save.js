55061330c07696dc9b3b96d6b9f896c3606e2121
gelos12 <gelos2012@gmail.com>
2018-09-05 16:38:28 +0900

    아이디저장

--- coinsense/static/id_save.js
+++ coinsense/static/id_save.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     // 저장된 쿠키값을 가져와서 ID 칸에 넣어준다. 없으면 공백으로 들어감.
     var userInputId = getCookie("userInputId");
-    $("input[name='id_username']").val(userInputId); 
+    $("#id_username").val(userInputId); 
      
-    if($("input[name='id_username']").val() != ""){ // 그 전에 ID를 저장해서 처음 페이지 로딩 시, 입력 칸에 저장된 ID가 표시된 상태라면,
+    if($("#id_username").val() != ""){ // 그 전에 ID를 저장해서 처음 페이지 로딩 시, 입력 칸에 저장된 ID가 표시된 상태라면,
         $("#id_save").attr("checked", true); // ID 저장하기를 체크 상태로 두기.
     }
      
     $("#id_save").change(function(){ // 체크박스에 변화가 있다면,
         if($("#id_save").is(":checked")){ // ID 저장하기 체크했을 때,
-            var userInputId = $("input[name='id_username']").val();
+            var userInputId = $("#id_username").val();
             setCookie("userInputId", userInputId, 7); // 7일 동안 쿠키 보관
         }else{ // ID 저장하기 체크 해제 시,
             deleteCookie("userInputId");
@@ -17,9 +17,9 @@ $(document).ready(function(){
     });
      
     // ID 저장하기를 체크한 상태에서 ID를 입력하는 경우, 이럴 때도 쿠키 저장.
-    $("input[name='id_username']").keyup(function(){ // ID 입력 칸에 ID를 입력할 때,
+    $("#id_username").keyup(function(){ // ID 입력 칸에 ID를 입력할 때,
         if($("#id_save").is(":checked")){ // ID 저장하기를 체크한 상태라면,
-            var userInputId = $("input[name='id_username']").val();
+            var userInputId = $("#id_username").val();
             setCookie("userInputId", userInputId, 7); // 7일 동안 쿠키 보관
         }
     });
