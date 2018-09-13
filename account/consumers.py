34d0bddc5f00ff1ecf3fa24a958d784cffece078
gelos12 <gelos2012@gmail.com>
2018-09-13 21:09:07 +0900

    최적화

--- account/consumers.py
+++ account/consumers.py
@@ -6,12 +6,9 @@ class NoticeConsumer(AsyncWebsocketConsumer):
         await self.accept()
         self.user_name = self.scope['url_route']['kwargs']['user_name']
         await self.channel_layer.group_add(self.user_name, self.channel_name)
-        print(f"added {self.channel_name} channel to {self.user_name}")
 
     async def disconnect(self,close_code):
-        await self.channel_layer.group_discard("gossip", self.channel_name)
-        print(f"Removed {self.channel_name} channel from gossip {close_code}")
-
+        await self.channel_layer.group_discard(self.user_name, self.channel_name)
+        
     async def user_event(self, event):
         await self.send(text_data=json.dumps(event,ensure_ascii=False))
-        print(f"Got message{event} at {self.channel_name}")
